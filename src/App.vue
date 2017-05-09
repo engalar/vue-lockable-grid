3c6a4aebafde58cc3b403ffdeeb91b6810afc514
engalar <liuwengao@foxmail.com>
2017-05-09 23:19:06 +0800

    enhancement scroll feature

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
   <div id="app">
-    <md-menu md-align-trigger>
-      <md-button md-menu-trigger>Align trigger</md-button>
-
-      <md-menu-content md-offset-y="-90">
-        <md-menu-item>My Item 1</md-menu-item>
-        <md-menu-item>My Item 2</md-menu-item>
-        <md-menu-item>My Item 3</md-menu-item>
-      </md-menu-content>
-    </md-menu>
-
     <router-view></router-view>
   </div>
 </template>
@@ -27,6 +17,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <h1>Top:{{top}}</h1>
-    <div style="position: relative;height: 150px;width: 1000px;overflow: hidden">
-      <div id="locked-table" style="vertical-align: top;overflow: hidden; display: inline-block;width: 492px;height: 150px;box-sizing: content-box">
+    <div style="position: relative;height: 450px;width: 100%;overflow: hidden;display: flex">
+      <div id="locked-table"
+           style="vertical-align: top;overflow: hidden; display: inline-block;width: 492px;height: 350px;box-sizing: content-box">
         <ics-table :columns="gridColumns" :filterKey="searchQuery" :data="gridData"></ics-table>
       </div>
-      <div id="srollable-table" v-on:scroll="onScroll($event)" style="width: 152px;height: 167px;box-sizing: content-box;display: inline-block;overflow: scroll">
+      <div id="srollable-table" v-on:scroll="onScroll($event)"
+           style="width: 30%;height: 367px;box-sizing: content-box;display: inline-block;overflow: scroll">
         <ics-table :columns="gridColumns" :filterKey="searchQuery" :data="gridData"></ics-table>
       </div>
     </div>
+    <button v-on:click="change">Delete first two row</button>
+    <button v-on:click="insert">Insert Two row after first row</button>
   </div>
 
 </template>
@@ -33,6 +37,9 @@
         {name: 'Jackie Chan', power: 7000},
         {name: 'Jet Li', power: 8000}
       ]
+      for (let i = 0; i < 2; i++) {
+        gridData.push({name: 'Chuck Norris' + i, power: Infinity, plan: 38, out: 12})
+      }
       return {
         vertical: 'bottom',
         horizontal: 'center',
@@ -56,6 +63,12 @@
         console.log(e.target.scrollTop)
         this.top = $('#srollable-table').scrollTop()
         $('#locked-table').scrollTop($('#srollable-table').scrollTop())
+      },
+      change () {
+        this.gridData.splice(0, 2)
+      },
+      insert () {
+        this.gridData.splice(1, 0, {name: 'Chuck Norris' + this.gridData.length, power: Infinity, plan: 38, out: 12})
       }
     },
     computed: {
